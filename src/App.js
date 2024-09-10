988a5c1fc9a89e69960dfee2bfaeefb507b11f2e
saad zafar <codewithsaadzafar@gmail.com>
2024-09-10 18:53:39 +0500

    updated routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import AddTransaction from './components/AddTransaction';
+import AddVendor from './components/AddVendor';
+import GetAllUsers from './components/GetAllUsers';
+import GetAllVendors from './components/GetAllVendors';
+import UpdateVendor from './components/UpdateVendor';
+import UpdateUser from './components/UpdateUser';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-
-
   return (
     <div className="App">
-     <Login />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/add-transaction" element={<AddTransaction />} />
+        <Route path="/add-vendor" element={<AddVendor />} />
+        <Route path="/update-transactions" element={<GetAllUsers />} />
+        <Route path="/update-vendors" element={<GetAllVendors />} />
+        <Route path="/update-users/:id" element={<UpdateUser />} />
+        <Route path="/update-vendors/:id" element={<UpdateVendor />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,57 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import Dashboard from './components/Dashboard';
-import AddTransaction from './components/AddTransaction';
-import AddVendor from './components/AddVendor';
-import GetAllUsers from './components/GetAllUsers';
-import GetAllVendors from './components/GetAllVendors';
-import UpdateVendor from './components/UpdateVendor';
-import UpdateUser from './components/UpdateUser';
+import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-let routes  = createBrowserRouter([
-  {
-    path:'/',
-    element: <App/>
-  },
-  {
-    path:'/dashboard',
-    element: <Dashboard />
-  },
-  {
-    path:'/add-transaction',
-    element: <AddTransaction />
-  },
-  {
-    path:'/add-vendor',
-    element: <AddVendor />
-  },
-  {
-    path:'/update-transactions',
-    element: <GetAllUsers />
-  },
-  {
-    path:'/update-vendors',
-    element: <GetAllVendors />
-  },
-  {
-    path:'/update-users/:id',
-    element: <UpdateUser />
-  },
-  {
-    path:'/update-vendors/:id',
-    element: <UpdateVendor />
-  },
-])
+
 root.render(
   <React.StrictMode>
-    <RouterProvider  router={routes} />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
